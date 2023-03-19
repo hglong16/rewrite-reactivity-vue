47eeff8a530a818b9906e61082968905bbf3dc47
hglong16 <intihad.vuong@gmail.com>
2023-03-19 15:02:08 +0700

    example 1 done

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-export const chunk = (arr: Array<any>, size: number) => {
-  return Array.from({ length: Math.ceil(arr.length / size) }, (_, i) => {
-    arr.slice(i * size, i * size + size);
-  });
-};
+// example
+
+import { reactive, watchEffect } from "vue";
+const MAX_ROAD_LENGTH = 10;
+
+const mod = (x: number, y: number) => ((x % y) + y) % y;
+
+const car = reactive({
+  position: 0,
+  speed: 2,
+});
+
+setInterval(() => {
+  car.position = mod(car.position + car.speed, MAX_ROAD_LENGTH);
+}, 1000);
+
+watchEffect(() => {
+  const road = [..."_".repeat(MAX_ROAD_LENGTH)];
+  road[car.position] = "🚗";
+  console.log(road.reverse().join(""));
+});
